f899b34754d9e10cba339db8de25d722ebeac798
alvarikola <alvaroe484@gmail.com>
2024-11-20 11:20:00 +0000

    añadido base de datos

--- app/src/main/java/com/example/tareasroom/AppDatabase.kt
+++ app/src/main/java/com/example/tareasroom/AppDatabase.kt
@@ -1,4 +1,24 @@
 package com.example.tareasroom
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [Tarea::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun tareaDao(): TareaDao
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "Tareas_database" ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
