631cc70d87c378d4674244d39ac6e06ebeb4d894
alvarikola <alvaroe484@gmail.com>
2024-11-20 11:19:44 +0000

    añadido DAO

--- app/src/main/java/com/example/tareasroom/TareaDao.kt
+++ app/src/main/java/com/example/tareasroom/TareaDao.kt
@@ -1,4 +1,26 @@
 package com.example.tareasroom
 
-interface TareaDao {
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+
+
+@Dao
+interface TareaDao : List<Tarea> {
+
+    @Insert
+    suspend fun insert(tarea: Tarea)
+
+    @Update
+    suspend fun update(tarea: Tarea)
+
+    @Delete
+    suspend fun delete(tarea: Tarea)
+
+    @Transaction
+    @Query("SELECT * FROM Tareas_database")
+    suspend fun getAllTareasAndTipos(): List<Tarea>
 }
