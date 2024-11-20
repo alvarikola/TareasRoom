6b3b919dd3f3c2e3f723bc0b18800808d659bbe1
alvarikola <alvaroe484@gmail.com>
2024-11-20 11:19:19 +0000

    tablas

--- app/src/main/java/com/example/tareasroom/Tarea.kt
+++ app/src/main/java/com/example/tareasroom/Tarea.kt
@@ -1,4 +1,31 @@
 package com.example.tareasroom
 
-class Tarea {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Embedded
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.Relation
+
+
+@Entity(tableName = "Tareas")
+data class Tarea(
+    @PrimaryKey(autoGenerate = true) val idTarea: Int,
+    @ColumnInfo(name = "Título") val tituloTarea: String,
+    @ColumnInfo(name = "Descripción") val descripcionTarea: String?,
+    val idTipoTareaOwner: Int
+)
+
+@Entity(tableName = "TiposTareas")
+data class TipoTarea(
+    @PrimaryKey val idTipoTarea: Int,
+    val tituloTipoTarea: String
+)
+
+data class TareasWithTipo(
+    @Embedded val tarea: Tarea,
+    @Relation(
+        parentColumn = "idTipoTarea",
+        entityColumn = "idTipoTareaOwner"
+    )
+    val tiposTareas: List<TipoTarea>
+)
