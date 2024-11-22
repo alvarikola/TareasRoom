c53334c9748fbbd7da6dce136e6028f27932740f
alvarikola <alvaroe484@gmail.com>
2024-11-22 10:39:48 +0000

    creada la tabla TipoTarea en su archivo correspondiente

---
+++ app/src/main/java/com/example/tareasroom/TipoTarea.kt
@@ -0,0 +1,10 @@
+package com.example.tareasroom
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "TiposTareas")
+data class TipoTarea(
+    @PrimaryKey(autoGenerate = true) val idTipoTarea: Int = 0,
+    val tituloTipoTarea: String
+)
