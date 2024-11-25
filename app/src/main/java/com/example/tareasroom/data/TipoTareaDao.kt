ccf40a2e8edd3c6ef307d8ce20d2af91dc9ef281
alvarikola <alvaroe484@gmail.com>
2024-11-25 13:40:51 +0000

    añadidos CRUD TipoTarea

--- app/src/main/java/com/example/tareasroom/data/TipoTareaDao.kt
+++ app/src/main/java/com/example/tareasroom/data/TipoTareaDao.kt
@@ -1,19 +1,24 @@
 package com.example.tareasroom.data
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 
 @Dao
 interface TipoTareaDao {
 
+    @Insert
+    suspend fun insertTipoTarea(tipoTarea: TipoTarea)
 
+    @Update
+    suspend fun update(tipoTarea: TipoTarea)
+
+    @Delete
+    suspend fun delete(tipoTarea: TipoTarea)
 
     @Query("SELECT * FROM TiposTareas")
     suspend fun getAllTipos(): List<TipoTarea>
-
-
-    @Insert
-    suspend fun insertTipoTarea(tipoTarea: TipoTarea)
 
 }
